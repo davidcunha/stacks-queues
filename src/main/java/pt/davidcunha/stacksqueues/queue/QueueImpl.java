fa0f0d7ea2333b71c42acdf31a3f12284a9ad13f
David Cunha <davidgoncalvescunha@gmail.com>
2013-05-15 22:57:05 +0100

    queue implementation skeleton

--- src/main/java/pt/davidcunha/stacksqueues/queue/QueueImpl.java
+++ src/main/java/pt/davidcunha/stacksqueues/queue/QueueImpl.java
@@ -9,5 +9,33 @@ package pt.davidcunha.stacksqueues.queue;
  */
 public class QueueImpl {
 
-  
+    private int max;
+    private int front;
+    private int rear;
+    private long[] queueArray;
+
+    public QueueImpl(int size) {
+        this.max = size;
+        this.front = 0;
+        this.rear = size - 1;
+        this.queueArray = new long[max];
+    }
+
+    public void insert() {
+    }
+
+    public long remove() {
+    }
+
+    public long peek() {
+    }
+
+    public boolean isFull() {
+    }
+
+    public boolean isEmpty() {
+    }
+
+    public int size() {
+    }
 }
