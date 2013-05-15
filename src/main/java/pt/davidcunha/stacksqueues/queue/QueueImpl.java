d9546ff825635713e7e9496a2b0d45ff536bbc8a
David Cunha <davidgoncalvescunha@gmail.com>
2013-05-16 00:25:06 +0100

    queue implementation

--- src/main/java/pt/davidcunha/stacksqueues/queue/QueueImpl.java
+++ src/main/java/pt/davidcunha/stacksqueues/queue/QueueImpl.java
@@ -1,5 +1,5 @@
 /**
- * Stack and Queues - A queue implementation
+ * Stack and Queues - A queue implementation with circular buffer support
  */
 package pt.davidcunha.stacksqueues.queue;
 
@@ -12,30 +12,78 @@ public class QueueImpl {
     private int max;
     private int front;
     private int rear;
+    private int nItems;
     private long[] queueArray;
 
+    /**
+     * QueueImpl public constructor
+     *
+     * @param size array initialization
+     */
     public QueueImpl(int size) {
         this.max = size;
         this.front = 0;
-        this.rear = size - 1;
+        this.rear = -1;
         this.queueArray = new long[max];
+        this.nItems = 0;
     }
 
-    public void insert() {
+    /**
+     * Insert new element in the queue
+     *
+     */
+    public void insert(long n) {
+        if (this.rear == this.max - 1) {
+            this.rear = -1;
+        }
+        this.rear++;
+        this.queueArray[this.rear] = n;
+        this.nItems++;
     }
 
+    /**
+     * Remove front of the queue
+     *
+     * @return first element
+     */
     public long remove() {
+        long temp = this.queueArray[this.front];
+        this.front++;
+        if (this.front == this.max) {
+            this.front = 0;
+        }
+        this.nItems--;
+        return temp;
     }
 
+    /**
+     * Queue peek front of the queue
+     *
+     * @return first element
+     */
     public long peek() {
+        return this.queueArray[front];
     }
 
-    public boolean isFull() {
-    }
-
+    /**
+     * Queue is empty?
+     *
+     * @return true or false
+     */
     public boolean isEmpty() {
+        return (this.nItems == 0);
+    }
+
+    /**
+     * Queue is full?
+     *
+     * @return true or false
+     */
+    public boolean isFull() {
+        return (this.nItems == this.max);
     }
 
     public int size() {
+        return this.nItems;
     }
 }
